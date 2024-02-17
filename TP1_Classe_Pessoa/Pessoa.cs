90c63dddb2905a55c8bba41085810f9d14dd9510
diego <diego@DIEGO_PC>
2024-02-17 15:36:35 -0300

    Adicionar arquivos de projeto.

---
+++ TP1_Classe_Pessoa/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1_Classe_Pessoa
+{
+    internal class Pessoa
+    {
+        private string _nome;
+        private int _idade;
+        private string _cpf;
+
+        public Pessoa()
+        {
+
+
+        }
+        public Pessoa(string nome, int idade, string cpf)
+        {
+            Nome = nome;
+            Idade = idade;
+            Cpf = cpf;
+
+        }
+
+        public string Nome { get => _nome; set => _nome = value; }
+        public int Idade { get => _idade; set => _idade = value; }
+        public string Cpf { get => _cpf; set => _cpf = value; }
+    }
+}
---
+++ TP1_Classe_Pessoa/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Reflection;
+
+namespace TP1_Classe_Pessoa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa();
+            p1.Nome = "Diego";
+            p1.Idade = 34;
+            p1.Cpf = "123.456.789-10";
+            Console.WriteLine("Nome: " + p1.Nome);
+            Console.WriteLine("Idade: " + p1.Idade);
+            Console.WriteLine("CPF: " + p1.Cpf);
+            Console.WriteLine("---------------------");
+
+
+            string opcao;
+            List <Pessoa> list = new List<Pessoa>();
+
+            do
+            {
+                Console.WriteLine("\n" +
+                    "=-=-= MENU -=-=-=\n" +
+                    "\n" +
+                    "1. Adicionar pessoa\n" +
+                    "2. Imprimir lista\n" +
+                    "3. sair");
+                opcao = Console.ReadLine();
+
+                switch (opcao) 
+                {
+                    case "1":
+                        Console.WriteLine("Nome: ");
+                        string nome = Console.ReadLine();
+                        Console.WriteLine("Idade: ");
+                        int idade = int.Parse(Console.ReadLine());
+                        Console.WriteLine("CPF: ");
+                        string cpf = Console.ReadLine();
+                        Pessoa pessoa = new Pessoa(nome, idade, cpf);
+                        list.Add(pessoa);
+                        break;
+
+                    case "2":
+                        foreach (var item in list)
+                        {
+                            Console.WriteLine($"Nome: {item.Nome}\n" + $"Idade: {item.Idade}\n" + $"CPF: {item.Cpf}\n" + $"-------------------");
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("Fim.");
+                        break;
+             
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+
+                }
+
+
+
+            } while (opcao != "3");
+        }
+    }
+}
